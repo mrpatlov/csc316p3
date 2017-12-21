3659cebc5b01463dd3cfa3668ea355ce42d8612c
mrpatlov <34523630+mrpatlov@users.noreply.github.com>
2017-12-21 15:51:32 -0500

    Update Network.java

--- src/Network.java
+++ src/Network.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
  * 
  * Note:efficancy can be strongly improved by sorting the lists after entries added then
  * using bianary searchs for Verticies rather than iterators
- *
+ *Took contributors names out for identity purposes
  */
 public class Network {
 	
